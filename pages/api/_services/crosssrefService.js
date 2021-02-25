aa7a1a9d2860a0846952e4545400221e77fed544
gadnlinoo <guiavenas@gmail.com>
2021-02-25 08:45:10 -0300

    Adicionando método queryWorksByContributor ao crossrefService

--- pages/api/_services/crosssrefService.js
+++ pages/api/_services/crosssrefService.js
@@ -14,5 +14,18 @@ export default {
 		const res = await axios.get(endpoint, { params });
 
 		return res.data;
+	},
+
+	queryWorksByContributor: async (contributor) => {
+		let params = {
+			"query.contributor": contributor,
+			"mailto": process.env.CROSSREF_USER_AGENT
+		};
+
+		const endpoint = apiBaseUrl + "/works";
+		
+		const res = await axios.get(endpoint, { params });
+
+		return res.data;
 	}
 }
